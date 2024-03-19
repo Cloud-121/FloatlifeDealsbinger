7515572762bc5e95bdd25c5dcbfb23f2627ca68d
Lenley Ngo <72272909+Cloud-121@users.noreply.github.com>
2024-03-18 20:20:29 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,26 +4,24 @@ import time
 import json
 
 global present
-#Set to true to send messege for deal of day, Set to false to ignore previous
 present = True
 
-# Function to send Discord webhook message
 def send_webhook_message(Namer, product_name, product_image, link):
-    webhook_url = 'WEBHOOK'
+    webhook_url = 'https://discord.com/api/webhooks/1219439917038305290/IhJreQphZKuYmQw_InEQkkgmx84ZBdhncNfwwc41ge0ifUQtpfiFqsU8H0-sd4YgsZUn'
 
     data = {
-    "username" : "Vesc Deals"
+        "username": "Vesc Deals"
     }
 
     data["embeds"] = [
         {
-        "description" : f"[{product_name}]({link})",  # Added comma here
-        "title" : f"{Namer}",
-        "image": {"url": f"{product_image}"}
+            "description": f"[{product_name}]({link})",
+            "title": f"{Namer}",
+            "image": {"url": f"{product_image}"}
         }
     ]
 
-    result = requests.post(webhook_url, json = data)
+    result = requests.post(webhook_url, json=data)
     try:
         result.raise_for_status()
     except requests.exceptions.HTTPError as err:
@@ -31,9 +29,7 @@ def send_webhook_message(Namer, product_name, product_image, link):
     else:
         print("Payload delivered successfully, code {}.".format(result.status_code))
 
-# Function to get deal of the day products
 def get_deal_of_the_day_products():
-    print("Finding Day")
     url = 'https://thefloatlife.com/collections/deal-of-the-day'
     response = requests.get(url)
     if response.status_code == 200:
@@ -54,7 +50,6 @@ def get_deal_of_the_day_products():
     return None
 
 def jeffthriftshop():
-    print("Finding Jeff")
     url = 'https://thefloatlife.com/collections/jeffs-thrift-shop'
     response = requests.get(url)
     if response.status_code == 200:
@@ -74,56 +69,48 @@ def jeffthriftshop():
         return products
     return None
 
-# Function to continuously check for changes in product names and send webhook
 def monitor_deals():
     previous_names = set()
 
     while True:
-
-	#The Float Life Section
-        #Deal of day
         deal_of_the_day_products = get_deal_of_the_day_products()
         if deal_of_the_day_products:
             for product in deal_of_the_day_products:
                 if product['name'] not in previous_names:
-                    if (present): 
+                    if present:
                         print(f"Preset set to True")
                         send_webhook_message("New Deal Of The Day From The Float Life!!", product['name'], product['image'], "https://thefloatlife.com/collections/deal-of-the-day")
                         previous_names.add(product['name'])
                     else:
                         print(f"Present was set to False")
-                        preset = True
-                        print(f"Preset set to {preset}")
+                        present = True
+                        print(f"Preset set to {present}")
                         print("adding previous")
                         previous_names.add(product['name'])
                         print("Added prev")
                 else:
                     print("Same as before")
 
-        #Thrift Shop
         thriftshop = jeffthriftshop()
         if thriftshop:
             for product in thriftshop:
                 if product['name'] not in previous_names:
-                    if (present): 
+                    if present:
                         print(f"Preset set to True")
                         send_webhook_message("New Thrift From The Float Life!!", product['name'], product['image'], "https://thefloatlife.com/collections/jeffs-thrift-shop")
                         previous_names.add(product['name'])
                     else:
                         print(f"Present was set to False")
-                        preset = True
-                        print(f"Preset set to {preset}")
+                        present = True
+                        print(f"Preset set to {present}")
                         print("adding previous")
                         previous_names.add(product['name'])
                         print("Added prev")
                 else:
                     print("Same as before")
                     
-
-	#More could be added
         print("waiting")
-        time.sleep(60)  # Check every 60 seconds
+        time.sleep(60)
         print("Running again")
 
-# Start monitoring the deal of the day
 monitor_deals()
